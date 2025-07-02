356ae72a5da08c1e4fa6498c73340247f8df4cf2
wjqhydn <1179580830@qq.com>
2025-07-02 18:03:26 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["2qk75910hm95.vicp.fun"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["wjqhydn.cn"];
 pub const PUBLIC_RS_PUB_KEY: &str = "O+tQaCCrqzuOY3ijhF8Av4QDCff3LjTBsiqoDRiLyeQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
